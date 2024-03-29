a64071d909e99b16411899b3900266d38dd0ae55
kolumber23 <122394298+kolumber23@users.noreply.github.com>
2024-03-29 23:41:58 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ShoppingListDetail from './ShoppingListDetail';
+import MainPage from './MainPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>Main Page</div>} />
+        <Route path="/" element={<MainPage />} /> {/* Use MainPage here */}
         <Route path="/shopping-list/:listId" element={<ShoppingListDetail />} />
       </Routes>
     </Router>
